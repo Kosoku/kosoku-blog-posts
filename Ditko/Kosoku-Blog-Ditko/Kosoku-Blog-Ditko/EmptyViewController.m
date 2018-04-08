dd94465a139bc3fb3428d9ef996cc12be9dae1cd
William Towe <willbur1984@gmail.com>
2018-04-08 03:46:44 -0400

    add KDIEmptyView example

--- Ditko/Kosoku-Blog-Ditko/Kosoku-Blog-Ditko/EmptyViewController.m
+++ Ditko/Kosoku-Blog-Ditko/Kosoku-Blog-Ditko/EmptyViewController.m
@@ -1,8 +1,8 @@
 //
-//  TabBarController.m
+//  EmptyViewController.m
 //  Kosoku-Blog-Ditko
 //
-//  Created by William Towe on 4/6/18.
+//  Created by William Towe on 4/8/18.
 //  Copyright © 2018 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,31 +13,49 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import "TabBarController.h"
-#import "BorderedViewController.h"
-#import "BadgeViewController.h"
-#import "UINavigationController+Extensions.h"
-#import "WindowViewController.h"
-#import "ButtonViewController.h"
 #import "EmptyViewController.h"
+#import "Constants.h"
 
-@interface TabBarController ()
+#import <KSOFontAwesomeExtensions/KSOFontAwesomeExtensions.h>
+#import <Ditko/Ditko.h>
 
+@interface EmptyViewController ()
+@property (strong,nonatomic) KDIEmptyView *emptyView;
 @end
 
-@implementation TabBarController
+@implementation EmptyViewController
+
+- (NSString *)title {
+    return @"KDIEmptyView";
+}
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     if (!(self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
         return nil;
     
-    self.viewControllers = @[[UINavigationController KSO_navigationControllerWithViewController:[[BorderedViewController alloc] initWithNibName:nil bundle:nil]],
-                             [UINavigationController KSO_navigationControllerWithViewController:[[BadgeViewController alloc] initWithNibName:nil bundle:nil]],
-                             [UINavigationController KSO_navigationControllerWithViewController:[[WindowViewController alloc] initWithNibName:nil bundle:nil]],
-                             [UINavigationController KSO_navigationControllerWithViewController:[[ButtonViewController alloc] initWithNibName:nil bundle:nil]],
-                             [UINavigationController KSO_navigationControllerWithViewController:[[EmptyViewController alloc] initWithNibName:nil bundle:nil]]];
+    self.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Empty" image:[UIImage KSO_fontAwesomeSolidImageWithString:@"\uf2cb" size:kBarButtonItemImageSize].KDI_templateImage selectedImage:nil];
     
     return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = UIColor.whiteColor;
+    
+    self.emptyView = [[KDIEmptyView alloc] initWithFrame:CGRectZero];
+    self.emptyView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.emptyView.image = [UIImage KSO_fontAwesomeSolidImageWithString:@"\uf2cb" size:CGSizeMake(128, 128)].KDI_templateImage;
+    self.emptyView.headline = @"Empty Headline";
+    self.emptyView.body = @"Empty body text";
+    self.emptyView.action = @"Toggle activity indicator";
+    self.emptyView.actionBlock = ^(__kindof KDIEmptyView * _Nonnull emptyView) {
+        emptyView.loading = !emptyView.isLoading;
+    };
+    [self.view addSubview:self.emptyView];
+    
+    [NSLayoutConstraint activateConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[view]|" options:0 metrics:nil views:@{@"view": self.emptyView}]];
+    [NSLayoutConstraint activateConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[view]|" options:0 metrics:nil views:@{@"view": self.emptyView}]];
 }
 
 @end
